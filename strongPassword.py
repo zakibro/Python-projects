773812fc714213dad6a0c924b550312df8bda81d
zakibro <48967550+zakibro@users.noreply.github.com>
2019-03-26 22:23:33 +0100

    Update strongPassword.py

--- strongPassword.py
+++ strongPassword.py
@@ -1,16 +1,16 @@
 
 import re
 
-print('Podaj hasło:')
+print('Type Password:')
 text = input()
 
 def eightChar(text):
 	passwordRegex = re.compile(r'\w{8}')
 	mo = passwordRegex.search(text)
 	if mo == None:
-		print('Hasło nie ma 8 znaków')
+		print("Password hasn't got 8 charactes")
 	else:
-		print('Hasło ma 8 znaków')
+		print("Password has got 8 characters")
 		letters(text)
 
 def letters(text):
@@ -19,15 +19,15 @@ def letters(text):
 	if mo1 != None:
 		numbers(text)
 	else:
-		print('Słabe hasło')
+		print('Weak password')
 
 def numbers(text):
 	passwordRegex = re.compile(r'[0-9]+')
 	mo2 = passwordRegex.search(text)
 	if mo2 != None:
-		print('Mocne hasło')
+		print('Strong password')
 	else:
-		print('Słabe hasło')
+		print('Weak password')
 	
 		
 	
