2e0e792dd11c2ed420924243283ae3d151ba2cef
zakibro <48967550+zakibro@users.noreply.github.com>
2019-05-15 22:37:08 +0200

    Add files via upload

---
+++ decrypter.py
@@ -0,0 +1,27 @@
+#! python3
+
+import sys
+import os
+import PyPDF2
+
+password = sys.argv[1]
+
+
+for folderName, subfolders, filenames in os.walk('C://Users//PabloD//Desktop//python_projects'):
+    for filename in filenames:
+        if filename.endswith('_encrypted.pdf'):
+            pdfReader = PyPDF2.PdfFileReader(open(os.path.join(folderName, filename), 'rb'))
+            try:
+                pdfReader.decrypt(password)
+                pdfWriter = PyPDF2.PdfFileWriter()
+                for pageNum in range(pdfReader.numPages):
+                    pdfWriter.addPage(pdfReader.getPage(pageNum))
+                filename = filename.strip('_encrypted.pdf')
+                resultPdf = open(filename + 'new' + '.pdf', 'wb')
+                pdfWriter.write(resultPdf)
+            except:
+                print('Złe hasło')
+                continue
+
+
+
