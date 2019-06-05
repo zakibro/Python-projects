38dea5866df65294bff05b8d4a725a3bc4759f0f
zakibro <48967550+zakibro@users.noreply.github.com>
2019-06-05 20:00:48 +0200

    Add files via upload

---
+++ whereMouse.py
@@ -0,0 +1,18 @@
+#! python3
+
+# shows the current position of the mouse
+
+import pyautogui
+
+print('Press Ctrl + C to terminate the program')
+
+#loop
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\nDone!')
