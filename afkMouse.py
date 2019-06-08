aa71c2e2cd53d0b06ec4cdd0c8a219847e7cff28
zakibro <48967550+zakibro@users.noreply.github.com>
2019-06-08 11:41:05 +0200

    Add files via upload

---
+++ afkMouse.py
@@ -0,0 +1,13 @@
+#! python3
+
+import pyautogui
+import time
+
+while True:
+    try:
+        pyautogui.moveRel(10, 0, duration=0.25)
+        pyautogui.moveRel(-10, 0, duration=0.25)
+        time.sleep(10)
+    except KeyboardInterrupt:
+        print('Nareczka')
+        break
